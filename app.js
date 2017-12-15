05c1c8a1cb143697df7ed7ac222c9dda95bcf2b2
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-12-15 10:14:53 -0800

    send request to API and console log response

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //adding JS
+		var apikey = "36zcs2fpun2vtymn2qsvz6kd";
+        var baseUrl = "http://data.tmsapi.com/v1.1";
+        var showtimesUrl = baseUrl + '/movies/showings';
+        var zipCode = "94530";
+        var d = new Date();
+        var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+         
+        $(document).ready(function() {         
+// send AJAX request
+           	$.ajax({
+	         	url: showtimesUrl,
+	            data: {	
+	            	startDate: today,
+	         	    zip: zipCode,
+	         	    jsonp: "dataHandler",
+	         	    api_key: apikey
+	        	},			
+         		dataType: "jsonp",
+         		// method: "GET",
+           	});
+// for some reason this doesn't work????
+           	// .done(function(response){
+           	// 	var results = resonse.data
+           	// 	console.log(results)
+           	// })
+
+         });
+//console log the data
+        function dataHandler(data){
+           		console.log(data)
+        }
