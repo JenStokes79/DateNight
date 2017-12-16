ef9e969899e57c644a7226c67dd68cf9b9b4bc2d
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-12-16 11:26:30 -0800

    testing

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 //adding JS
 
-		var apikey = "36zcs2fpun2vtymn2qsvz6kd";
+
+
+
+//Yelp AJAX Call
+
+		$(document).ready(function() {   
+		var settings = {
+		 "async": true,
+		 "crossDomain": true,
+		 "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=40.82783908257346&longitude=-74.10162448883057",
+		 "method": "GET",
+		 "headers": {
+		   "authorization": "Bearer S7Iu9qp-LWQHVALB4GweWmd0ngVavSMAcMatDXn6PFk6tXIFDWBa4uNxhawNkJllDGo5c5-JvrIjIBvf-581Y4jxpPpsQZKExlOMEtEgyC-4g4wq0zjxutTqeZY0WnYx",
+		 }
+		}
+		$.ajax(settings).done(function (response) {
+		 console.log(response);
+		})
+})
+
+
+
+
+// zip code button function
+
+function zipCode(argument) {
+	var zip = $(this).text()
+			var apikey = "36zcs2fpun2vtymn2qsvz6kd";
         var baseUrl = "http://data.tmsapi.com/v1.1";
         var showtimesUrl = baseUrl + '/movies/showings';
         var zipCode = "94530";
@@ -31,23 +58,6 @@
         function dataHandler(data){
            		console.log(data)
         }
+}
 
-
-//Yelp AJAX Call
-
-		$(document).ready(function() {   
-		var settings = {
-		 "async": true,
-		 "crossDomain": true,
-		 "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=40.82783908257346&longitude=-74.10162448883057",
-		 "method": "GET",
-		 "headers": {
-		   "authorization": "Bearer S7Iu9qp-LWQHVALB4GweWmd0ngVavSMAcMatDXn6PFk6tXIFDWBa4uNxhawNkJllDGo5c5-JvrIjIBvf-581Y4jxpPpsQZKExlOMEtEgyC-4g4wq0zjxutTqeZY0WnYx",
-		 }
-		}
-		$.ajax(settings).done(function (response) {
-		 console.log(response);
-		})
-})
-
-
+console.log('this is from text branch')
