661b16782af14cf38a7d5c596723a71b0982f14f
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2017-12-17 18:33:58 -0800

    working firebase

--- app.js
+++ app.js
@@ -38,25 +38,25 @@ function searchRestaurant(argument) {
 	console.log($('#sel2').val().trim().toLowerCase())
 }
 
-function searchMovie(argument) {
-	var apikey = "36zcs2fpun2vtymn2qsvz6kd";
-    var baseUrl = "http://data.tmsapi.com/v1.1";
-    var showtimesUrl = baseUrl + '/movies/showings';
-    var zipCode = $('#input-zipCode').val().trim();
-    var d = new Date();
-    var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
-  	$.ajax({
-       	url: showtimesUrl,
-      	data: {	
-        	  	startDate: today,
-       	 	   	zip: zipCode,
-       	   	 	jsonp: "dataHandler",
-       	    	api_key: apikey,
-      		   },			
-    	dataType: "jsonp",
-     		// method: "GET",
-       	});
-}
+// function searchMovie(argument) {
+// 	var apikey = "36zcs2fpun2vtymn2qsvz6kd";
+//     var baseUrl = "http://data.tmsapi.com/v1.1";
+//     var showtimesUrl = baseUrl + '/movies/showings';
+//     var zipCode = $('#input-zipCode').val().trim();
+//     var d = new Date();
+//     var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+//   	$.ajax({
+//        	url: showtimesUrl,
+//       	data: {	
+//         	  	startDate: today,
+//        	 	   	zip: zipCode,
+//        	   	 	jsonp: "dataHandler",
+//        	    	api_key: apikey,
+//       		   },			
+//     	dataType: "jsonp",
+//      		// method: "GET",
+//        	});
+// }
 
 function dataHandler(data){
 	console.log(data)
@@ -67,10 +67,42 @@ $(document).ready(function() {
 	$(document).on('click', '#search', searchRestaurant)
 	// $(document).on('click', '#moviesearch', searchMovie)
 	// $(document).on('click', '#search', searchMovie)
-	event.preventDefault();
-
-	
-
+	//event.preventDefault();
 
 })
+
+//added a click buttin for when the user chooses to select their date night combo
+//once you click selct the data will push to firebase and the table above
+$("#select").on("click", function(event) {
+var movie = $("#movieResult").val();
+var restaurant = $("#yelpResults").val();
+
+var dateNight = {
+	movie: movie,
+	restaurant: restaurant
+}
+
+ database.ref().push(dateNight);
+
+ console.log(dateNight.movie); //data going to firebase but is " ". i think once we push the results it will show 
+ console.log(dateNight.restaurant); //data going to firebase but is " ". i think once we push the results it will show 
+
+
+database.ref().on("child_added", function(childSnapshot, prevChildKay){
+
+console.log(childSnapshot.val()); 
+
+
+
+var movie = childSnapshot.val().movie;
+var restaurant = childSnapshot.val().restaurant;
+
+  //pushes selected results into table
+ $("#dateNightInfo > tbody").append("<tr><td>" + movie + "</td><td>" + restaurant + "</td><td>");
+});
+
+console.log("FB is working")
+})
+//firebase is working, just need to figure out how to select results.
+
 
